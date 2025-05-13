7b7b9f2222a30c2c4175cf27f4a5a645e6f403d3
Vishal <72310075+Vishal-4323@users.noreply.github.com>
2025-05-13 15:34:39 +0530

    Update log_archive.sh
    added the changes to store the archive file in s3 bucket and remove the archive file in local system

--- log_archive.sh
+++ log_archive.sh
@@ -4,4 +4,12 @@ DATE=$(date +"%Y%m%d_%H%M%S")
 
 # read -p "Enter the log directory need to archive: " log_directory
 
-tar -cvzf archive_log_$DATE.tar.gz $1/*.log
+LOG_ARCHIVE_FILE_NAME=archive_log_$DATE.tar.gz
+
+tar -cvzf $LOG_ARCHIVE_FILE_NAME $1/*.log
+
+# This command copy the archive file from local to s3 bucket
+aws s3 cp $LOG_ARCHIVE_FILE_NAME s3://log-archive-200/$LOG_ARCHIVE_FILE_NAME
+
+# After copied the folder in s3 bucket this will remove the archive file in local
+rm $LOG_ARCHIVE_FILE_NAME
